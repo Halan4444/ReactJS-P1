8af1b26d622a4df3ca24ba11137bad066182e39d
Trần Hạ Lan <halan4444b@gmail.com>
2022-12-15 09:55:32 +0700

    first

---
+++ src/component/BaiTapChiaLayout/BaiTapChiaLayout.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import HeaderDemo from "./HeaderDemo";
+import Product from "./Product";
+import Home from "./Home";
+
+class BaiTapChiaLayout extends Component {
+    render() {
+        return (
+            <div>
+             <Home/>
+            </div>
+        );
+    }
+}
+
+export default BaiTapChiaLayout;
---
+++ src/component/demo/HeaderRFC.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HeaderRfc(props) {
+    return (
+        <div>
+            <h3>
+                React Header Component
+            </h3>
+        </div>
+    );
+}
+
+export default HeaderRfc;
